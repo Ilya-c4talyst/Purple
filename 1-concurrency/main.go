33e6e93b5e75c83c5ef86cbf716ea8c36c30c1e0
Ilya Savchenko <c4talyst12@yandex.ru>
2025-06-29 18:23:25 +0300

    first done

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Создание слайса
+func createSlice(lenght int, output chan<- int) {
+	defer close(output)
+	for range lenght {
+		output <- rand.Intn(101)
+	}
+}
+
+// Возведение в квадрат
+func square(input <-chan int, output chan<- int) {
+	defer close(output)
+	for value := range input {
+		output <- value * value
+	}
+}
+
+func main() {
+
+	// Количество элементов
+	lenght := 10
+	// Инит необходимых каналов
+	createCh := make(chan int, lenght)
+	squareCh := make(chan int, lenght)
+
+	// Pipeline
+	go createSlice(lenght, createCh)
+	go square(createCh, squareCh)
+
+	results := make([]int, lenght)
+	i := 0
+
+	for value := range squareCh {
+
+		// Печать результата
+		if i == 0 {
+			fmt.Print(value)
+		} else {
+			fmt.Print(" ", value)
+		}
+
+		// Сохранение в слайс
+		results[i] = value
+		i++
+	}
+}
