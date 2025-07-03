c5d994987c96f106e476cde17f35edc0a974ab1f
Ilya Savchenko <c4talyst12@yandex.ru>
2025-07-04 01:34:00 +0300

    done 3

--- 3-validation-api/pkg/utils/utils.go
+++ 3-validation-api/pkg/utils/utils.go
@@ -24,6 +24,7 @@ func SendMail(emailTo string, hash string, emailOut string, password string, add
 	e.From = "<" + emailOut + ">"
 	e.To = []string{emailTo}
 	e.Text = []byte("Перейдите по ссылке для подтверждения: http://localhost:8080/verify/" + hash)
+	// EmailOut - отправитель, password - пароль SMTP, address - SMTP address
 	err := e.Send(address+":587", smtp.PlainAuth("", emailOut, password, address))
 
 	if err != nil {
